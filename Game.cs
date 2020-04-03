6450f300574a872bb172d19e9ad29f7c9dffdbfc
Thore Lindqvist <thorelindq@gmail.com>
2020-04-03 15:09:17 +0200

    Version 0.1, 2020-04-03

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fyra_i_rad
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+    }
+}
